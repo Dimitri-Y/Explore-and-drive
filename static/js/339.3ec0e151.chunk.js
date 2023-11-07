56e7be791aa2847e0e5ee01eeb53ffba8c9d441f
Dimitri-Y <94981364+Dimitri-Y@users.noreply.github.com>
2023-11-07 19:34:39 +0000

    Deploying to gh-pages from @ Dimitri-Y/Explore-and-drive@d54475f129a0af611cb12bbbfb269b19febc5f35 🚀

---
+++ static/js/339.3ec0e151.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkexplore_and_drive=self.webpackChunkexplore_and_drive||[]).push([[339],{2339:function(e,n,r){r.r(n),r.d(n,{default:function(){return s}});var t=r(1906),i=r(184);function s(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("h1",{style:{textAlign:"center",margin:"32px"},children:"favorite cars"}),(0,i.jsx)(t.Z,{option:"favorites"}),";"]})}}}]);
+//# sourceMappingURL=339.3ec0e151.chunk.js.map
