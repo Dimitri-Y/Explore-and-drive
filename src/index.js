cd711b0ddf73493c15a7ff4f2afc794882a78fda
Dimitri-Y <dimoon.bond@gmail.com>
2023-11-07 17:06:52 +0200

    fix homepage route

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="Explore-and-drive">
+      <BrowserRouter basename="/Explore-and-drive">
         <App />
       </BrowserRouter>
     </Provider>
