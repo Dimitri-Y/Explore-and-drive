f9b5ee12f752258d110df1a78b6ff235e0b39955
Dimitri-Y <94981364+Dimitri-Y@users.noreply.github.com>
2024-06-02 19:22:28 +0000

    Deploying to gh-pages from @ Dimitri-Y/Explore-and-drive@d2e79b4e1629affb7640a986726c5c9e4ed4321d 🚀

---
+++ static/js/415.c02791c8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkexplore_and_drive=self.webpackChunkexplore_and_drive||[]).push([[415],{5415:(e,n,r)=>{r.r(n),r.d(n,{default:()=>i});var a=r(5558),s=r(184);function i(){return(0,s.jsxs)("div",{children:[(0,s.jsxs)(a.Fb,{children:["Welcome",(0,s.jsx)("span",{role:"img","aria-label":"Greeting icon"})]}),(0,s.jsx)(a.kI,{children:"Our application allows you to conveniently search and rent cars. We offer a wide selection of cars of various makes and models to suit your needs and budget."})]})}}}]);
+//# sourceMappingURL=415.c02791c8.chunk.js.map
